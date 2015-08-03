95dd2ede025551ffc3a4aba0b4b9774ae4f1ce04
Jon Packer <contact@jonpacker.com>
2015-08-03 16:56:13 +0200

    precompile immediately, dont assume server start

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 var http = require('http');
 module.exports = function(server, filesToPrecompile) {
-  server.on('listening', function() {
-    var address = server.address();
-    filesToPrecompile.forEach(function(file) {
-      var request = http.request({
-        host: address.address,
-        port: address.port,
-        method: 'GET',
-        path: file
-      })
-      request.on('error', function() {});
-      request.end();
-    });
+  var address = server.address();
+  filesToPrecompile.forEach(function(file) {
+    var request = http.request({
+      host: address.address,
+      port: address.port,
+      method: 'GET',
+      path: file
+    })
+    request.on('error', function() {});
+    request.end();
   });
 }
