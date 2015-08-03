9766454587f9f5d006b9e4cc2dfc586a49661e06
Jon Packer <contact@jonpacker.com>
2015-08-03 16:48:43 +0200

    fix error swallowing

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ module.exports = function(server, filesToPrecompile) {
   server.on('listening', function() {
     var address = server.address();
     filesToPrecompile.forEach(function(file) {
-      http.request({
+      var request = http.request({
         host: address.address,
         port: address.port,
         method: 'GET',
-        path: file,
-                          //swallow errors
-      }).end().on('error', function() {});
+        path: file
+      })
+      request.on('error', function() {});
+      request.end();
     });
   });
 }
